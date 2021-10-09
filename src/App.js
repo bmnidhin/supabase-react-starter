5d97df377cfab3ecd965975d73fa5a0081a710f4
Nidhin BM <nidhinbm.bm@gmail.com>
2021-10-10 01:30:04 +0530

    supabase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = 'http://localhost:8000'
+const SUPABASE_ANON_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJhdWQiOiIiLCJzdWIiOiIiLCJSb2xlIjoicG9zdGdyZXMifQ.magCcozTMKNrl76Tj2dsM7XTl_YH0v0ilajzAvIlw3U'
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 function App() {
   return (
